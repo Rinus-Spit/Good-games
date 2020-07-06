ab7af1f3351744eea7b302e031a5993e5509ba8e
Rinus-Spit <61282170+Rinus-Spit@users.noreply.github.com>
2020-07-06 12:11:49 +0200

    added enforce_SSL

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Collection;
 use Illuminate\Pagination\LengthAwarePaginator;
+use Illuminate\Contracts\Routing\UrlGenerator;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -25,6 +26,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        if(env('ENFORCE_SSL', true)) {
+            $url->forceScheme('https');
+        }
         /**
          * Paginate a standard Laravel Collection.
          *
